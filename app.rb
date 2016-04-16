224c40deffc357011f933d053e5a9ec3c6b03414
Justin Barber <barber.justin@gmail.com>
2016-04-15 20:34:45 -0400

    params keys should be strings. inspect normalized email data posted

--- app.rb
+++ app.rb
@@ -7,17 +7,20 @@ $logger.formatter = proc do |severity, datetime, progname, msg|
   "#{severity} #{msg}"
 end
 
+
+ ["headers", "attachment2", "dkim", "content-ids", "to", "cc", "html", "from", "text", "sender_ip", "attachment1", "envelope", "attachments", "subject", "attachment-info", "charsets", "SPF"]
+
 class App < Sinatra::Base
   post '/' do
     email = request.body.read
     $logger.info(params.keys)
-    email = params[:email]
-    cc = params[:cc]
-    to = params[:to]
-    from = params[:from]
-    subject = params[:subject]
-    $logger.info(email)
-    $logger.info(params)
+    email = params["email"]
+    cc = params["cc"]
+    to = params["to"]
+    from = params["from"]
+    subject = params["subject"]
+    $logger.info(params["attachments"])
+    $logger.info(params["attachment-info"])
     status 200
     "Ok"
   end
